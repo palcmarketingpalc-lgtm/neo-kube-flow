62a4fda0430c87608c804f5a46cf57da6350bd0b
palcmarketingpalc-lgtm <palcmarketingpalc@gmail.com>
2025-11-10 14:39:04 +0530

    Update Disclaimer.tsx

--- src/pages/Disclaimer.tsx
+++ src/pages/Disclaimer.tsx
@@ -1,4 +1,4 @@
-import { Header } from "@/components/Header";
+import { MinimalHeader } from "@/components/MinimalHeader";
 import { Footer } from "@/components/Footer";
 
 export default function Disclaimer() {
